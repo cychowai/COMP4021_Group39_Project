e88cd21ba3316fc6c73585f4e5d05cd230ad4a76
cychowai <71240864+cychowai@users.noreply.github.com>
2022-05-02 16:28:33 +0800

    update env.

--- server.js
+++ server.js
@@ -1,1 +1,29 @@
+const express = require("express");
 
+const bcrypt = require("bcrypt");
+const fs = require("fs");
+const session = require("express-session");
+
+// Create the Express app
+const app = express();
+
+// Use the 'public' folder to serve static files
+app.use(express.static("public"));
+
+// Use the json middleware to parse JSON data
+app.use(express.json());
+
+// Use the session middleware to maintain sessions
+const gameSession = session({
+  secret: "game",
+  resave: false,
+  saveUninitialized: false,
+  rolling: true,
+  cookie: { maxAge: 300000 }
+});
+app.use(gameSession);
+
+// Use a web server to listen at port 8000
+app.listen(8000, () => {
+  console.log("The server has started...");
+});
