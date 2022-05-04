9f5516bb4e192de0952f7ac35f7220994060ed12
cychowai <71240864+cychowai@users.noreply.github.com>
2022-05-04 17:51:01 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require("express");
 
-//const session = require("express-session");
-
 // Create the Express app
 const app = express();
 
@@ -10,18 +8,6 @@ app.use(express.static("public"));
 
 // Use the json middleware to parse JSON data
 app.use(express.json());
-
-// Use the session middleware to maintain sessions
-/*
-const gameSession = session({
-  secret: "game",
-  resave: false,
-  saveUninitialized: false,
-  rolling: true,
-  cookie: { maxAge: 300000 }
-});
-app.use(gameSession);
-*/
 
 // Use a web server to listen at port 8000
 app.listen(8000, () => {
