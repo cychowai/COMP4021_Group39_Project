533c35bdf7dafdc1310e0aacd2eeb47d60ae1e31
cychowai <71240864+cychowai@users.noreply.github.com>
2022-05-08 16:51:44 +0800

    update style

--- public/scripts/registration.js
+++ public/scripts/registration.js
@@ -1,4 +1,4 @@
-const Registration = (function() {
+const Registration = (function () {
     // This function sends a register request to the server
     // * `username`  - The username for the sign-in
     // * `avatar`    - The avatar of the user
@@ -8,28 +8,21 @@ const Registration = (function() {
     //                 request is successful in this form `onSuccess()`
     // * `onError`   - This is a callback function to be called when the
     //                 request fails in this form `onError(error)`
-    const register = function(username, avatar, name, password, onSuccess, onError) {
-
-        //
-        // A. Preparing the user data
-        //
+    const register = function (username, avatar, name, password, onSuccess, onError) {
         const users = {
             "username": username,
-            "avatar": avatar, 
-            "name": name, 
+            "avatar": avatar,
+            "name": name,
             "password": password
         };
         const jsonItem = JSON.stringify(users);
 
-        //
-        // B. Sending the AJAX request to the server
-        //
-        fetch("/register", { 
+        fetch("/register", {
             method: "POST",
-            headers: { "Content-Type": "application/json"},
+            headers: { "Content-Type": "application/json" },
             body: jsonItem
-         })
-            .then((res) => res.json() )
+        })
+            .then((res) => res.json())
             .then((json) => {
                 //console.log("Successful!");
                 if (json.status == "success") {
@@ -42,14 +35,6 @@ const Registration = (function() {
             .catch((err) => {
                 console.log("Error!");
             });
-        //
-        // F. Processing any error returned by the server
-        //
-            //**done
-        //
-        // J. Handling the success response from the server
-        //
-            //**done
         // Delete when appropriate
         if (onError) onError("This function is not yet implemented.");
     };
--- public/scripts/socket.js
+++ public/scripts/socket.js
@@ -1,22 +1,22 @@
 
-const Socket = (function() {
+const Socket = (function () {
     // This stores the current Socket.IO socket
     let socket = null;
 
     // This function gets the socket from the module
-    const getSocket = function() {
+    const getSocket = function () {
         return socket;
     };
 
     // This function connects the server and initializes the socket
-    const connect = function() {
+    const connect = function () {
         socket = io();
 
         // Wait for the socket to connect successfully
         socket.on("connect", () => {
             // Get the online user list
             socket.emit("get users");
-            
+
         });
 
         // Set up the users event
@@ -62,36 +62,37 @@ const Socket = (function() {
 
         socket.on("refuse starting", () => {
             $("#someonePlaying").show();
-        })
+        });
     };
 
     // This function disconnects the socket from the server
-    const disconnect = function() {
+    const disconnect = function () {
         socket.disconnect();
         socket = null;
     };
 
     // This function sends a post message event to the server
-    const postMessage = function(content) {
+    const postMessage = function (content) {
         if (socket && socket.connected) {
             socket.emit("post message", content);
         }
     };
 
-    const sendingMessage = function(){
+    const sendingMessage = function () {
         socket.emit("sending message");
-    }
+    };
 
-    const newMoveSignal = function(playerNum, keyCode){
-        socket.emit("newMoveSignal",playerNum, keyCode);
-    }
+    const newMoveSignal = function (playerNum, keyCode) {
+        socket.emit("newMoveSignal", playerNum, keyCode);
+    };
 
-    const newStopSignal = function(playerNum, keyCode){
-        socket.emit("newStopSignal",playerNum, keyCode);
-    }
+    const newStopSignal = function (playerNum, keyCode) {
+        socket.emit("newStopSignal", playerNum, keyCode);
+    };
 
-    const startGame = function(totalPlayerNum){
+    const startGame = function (totalPlayerNum) {
         socket.emit("start game", totalPlayerNum);
-    }
-    return { getSocket, connect, disconnect, postMessage, sendingMessage, newMoveSignal, newStopSignal, startGame};
+    };
+
+    return { getSocket, connect, disconnect, postMessage, sendingMessage, newMoveSignal, newStopSignal, startGame };
 })();
