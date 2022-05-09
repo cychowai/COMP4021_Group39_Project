ab9d68ec699a4a86608b87c8a25f73e8790f54e6
Jimccy <71240864+cychowai@users.noreply.github.com>
2022-05-09 17:17:27 +0800

    edit the cheating mode (#16)
    * edit the cheating mode
    
    * comment keyup
    
    * update speed
    
    * tidying
    
    * eatdot and eatpowerdot
    
    * add collision checking
    
    * remove unused console.log and comments
    
    * tidying up
    
    * delete node modules
    
    * add err to error messages
    
    * remove console.out error
    
    * Update player.js
    
    * can move correctly, cannot move easily
    
    * fix console.out issue
    
    * can move normally
    
    * give boundary situation
    
    * Update player.js
    
    * Update player.js
    
    * Update player.js
    
    Co-authored-by: LeeWingPiu <99600383+LEE-Wing-Piu-Jimmy@users.noreply.github.com>

--- public/player.js
+++ public/player.js
@@ -1,42 +1,162 @@
 const Player = function (ctx, x, y, gameArea) {
     const sequences = {
-        /* Idling sprite sequences for facing different directions */
         idleLeft: { x: 84, y: 4, width: 20, height: 20, count: 3, timing: 2000, loop: false },
         idleUp: { x: 84, y: 44, width: 20, height: 20, count: 3, timing: 2000, loop: false },
         idleRight: { x: 84, y: 24, width: 20, height: 20, count: 3, timing: 2000, loop: false },
         idleDown: { x: 84, y: 64, width: 20, height: 20, count: 3, timing: 2000, loop: false },
 
-        /* Moving sprite sequences for facing different directions */
         moveLeft: { x: 4, y: 4, width: 20, height: 20, count: 2, timing: 50, loop: true },
         moveUp: { x: 4, y: 44, width: 20, height: 20, count: 2, timing: 50, loop: true },
         moveRight: { x: 4, y: 24, width: 20, height: 20, count: 2, timing: 50, loop: true },
         moveDown: { x: 4, y: 64, width: 20, height: 20, count: 2, timing: 50, loop: true }
     };
 
-    // This is the sprite object of the player created from the Sprite module.
     const sprite = Sprite(ctx, x, y);
 
-    // The sprite object is configured for the player sprite here.
     sprite.setSequence(sequences.idleDown)
         .setScale(2)
         .setShadowScale({ x: 0.75, y: 0.20 })
         .useSheet("src/images/pac_sprite.png");
 
-    // This is the moving direction, which can be a number from 0 to 4:
     // - `0` - not moving
     // - `1` - moving to the left
     // - `2` - moving up
     // - `3` - moving to the right
     // - `4` - moving down
     let direction = 0;
-
-    // This is the moving speed (pixels per second) of the player
     let speed = 20;
+    let moveBuffer = null;
 
-    // This function sets the player's moving direction.
-    // - `dir` - the moving direction (1: Left, 2: Up, 3: Right, 4: Down)
+    const isCollideWithWall = function (x, y, dir) {
+        if (dir === 0) {
+            return;
+        }
+
+        let row = y / tileSize - 0.5;
+        let column = x / tileSize - 0.5;
+        let nextRow, nextColumn = null;
+        let nextRow2, nextColumn2 = null;
+
+        switch (direction) {
+            case 0:
+                nextColumn = Math.round(column);
+                nextRow = Math.round(row);
+                if (dir === 1) {
+                    nextColumn--;
+                }
+                else if (dir === 2) {
+                    nextRow--;
+                }
+                else if (dir === 3) {
+                    nextColumn++;
+                }
+                else if (dir === 4) {
+                    nextRow++;
+                }
+                nextRow2 = nextRow;
+                nextColumn2 = nextColumn;
+                break;
+            case 1:
+                nextColumn = Math.ceil(column);
+                nextColumn2 = Math.floor(column + 0.1);
+                if (dir === 2) {//from left to up
+                    nextRow = row - 1;
+                }
+                else if (dir === 4) {//left to down
+                    nextRow = row + 1;
+                }
+                else if (dir === 1) {//left to left
+                    nextColumn--;
+                    nextRow = row;
+                }
+                else {
+                    nextRow = row;
+                }
+                nextRow2 = nextRow;
+                break;
+            case 2:
+                nextRow = Math.ceil(row);
+                nextRow2 = Math.floor(row + 0.1);
+                if (dir === 1) {//from up to left
+                    nextColumn = column - 1;
+                }
+                else if (dir === 3) {//up to right
+                    nextColumn = column + 1;
+                }
+                else if (dir === 4) {//up to up
+                    nextRow--;
+                    nextColumn = column;
+                }
+                else {
+                    nextColumn = column;
+                }
+                nextColumn2 = nextColumn;
+                break;
+            case 3:
+                nextColumn = Math.floor(column);
+                nextColumn2 = Math.ceil(column - 0.1);
+                if (dir === 2) {//from right to up
+                    nextRow = row - 1;
+                }
+                else if (dir === 4) {//right to down
+                    nextRow = row + 1;
+                }
+                else if (dir === 3) {//right to right
+                    nextColumn++;
+                    nextRow = row;
+                }
+                else {
+                    nextRow = row;
+                }
+                nextRow2 = nextRow;
+                break;
+            case 4:
+                nextRow = Math.floor(row);
+                nextRow2 = Math.ceil(row - 0.1);
+                if (dir === 1) {//from down to left
+                    nextColumn = column - 1;
+                }
+                else if (dir === 3) {//down to right
+                    nextColumn = column + 1;
+                }
+                else if (dir === 4) {//down to down
+                    nextRow++;
+                    nextColumn = column;
+                }
+                else {
+                    nextColumn = column;
+                }
+                nextColumn2 = nextColumn;
+                break;
+        }
+
+        let roundedRow = Math.round(nextRow);
+        let roundedColumn = Math.round(nextColumn);
+        let roundedRow2 = Math.round(nextRow2);
+        let roundedColumn2 = Math.round(nextColumn2);
+
+        /*
+        if (direction !== dir && direction !== 0
+            && (Math.abs(column - Math.floor(column)) < 0.1 || Math.abs(column - Math.floor(column)) > 0.9)
+            && (Math.abs(row - Math.floor(row)) < 0.1 || Math.abs(row - Math.floor(row)) > 0.9)) {
+            moveBuffer = dir;
+            return false;
+        }
+        */
+
+        if (map[roundedRow][roundedColumn] === 1 || map[roundedRow2][roundedColumn2] === 1) {
+            if (direction !== dir && direction !== 0)
+                moveBuffer = dir;
+            return true;
+        }
+
+        return false;
+    };
+
     const move = function (dir) {
-        if (dir >= 1 && dir <= 4 && dir != direction) {
+        let { x, y } = sprite.getXY();
+        if (dir >= 1 && dir <= 4 && dir !== direction
+            && !isCollideWithWall(x, y, dir)) {
             switch (dir) {
                 case 1: sprite.setSequence(sequences.moveLeft); break;
                 case 2: sprite.setSequence(sequences.moveUp); break;
@@ -44,19 +164,20 @@ const Player = function (ctx, x, y, gameArea) {
                 case 4: sprite.setSequence(sequences.moveDown); break;
             }
             direction = dir;
+
+            //clear the buffer once it is succesfully moved
+            if (dir === moveBuffer)
+                moveBuffer = null;
         }
     };
 
-    // This function stops the player from moving.
-    // - `dir` - the moving direction when the player is stopped (1: Left, 2: Up, 3: Right, 4: Down)
     const stop = function (dir) {
-        if (direction == dir) {
+        if (direction === dir) {
             switch (dir) {
                 case 1: sprite.setSequence(sequences.idleLeft); break;
                 case 2: sprite.setSequence(sequences.idleUp); break;
                 case 3: sprite.setSequence(sequences.idleRight); break;
                 case 4: sprite.setSequence(sequences.idleDown); break;
-
             }
             direction = 0;
         }
@@ -64,16 +185,36 @@ const Player = function (ctx, x, y, gameArea) {
 
     // This function speeds up the player.
     const speedUp = function () {
-        speed = 250;
+        speed = 40; //double the speed
     };
 
     // This function slows down the player.
     const slowDown = function () {
-        speed = 150;
+        speed = 20; //back to normal speed
     };
 
-    // This function updates the player depending on his movement.
-    // - `time` - The timestamp when this function is called
+    const eatDot = function (x, y) {
+        const row = Math.floor(y / tileSize);
+        const column = Math.floor(x / tileSize);
+        if (map[row][column] === 2) {
+            map[row][column] = 0;
+            //scores += 50;
+            return true;
+        }
+        return false;
+    };
+
+    const eatPowerDot = function (x, y) {
+        const row = Math.floor(y / tileSize);
+        const column = Math.floor(x / tileSize);
+        if (map[row][column] === 3) {
+            map[row][column] = 0;
+            //scores += 100;
+            return true;
+        }
+        return false;
+    };
+
     const update = function (time) {
         /* Update the player if the player is moving */
         if (direction != 0) {
@@ -90,10 +231,25 @@ const Player = function (ctx, x, y, gameArea) {
             /* Set the new position if it is within the game area */
             if (gameArea.isPointInBox(x, y))
                 sprite.setXY(x, y);
+
+            if (eatDot(x, y)) {
+                wakaSound.play();
+            }
+
+            if (eatPowerDot(x, y)) {
+                powerDotSound.play();
+            }
+
+            if (isCollideWithWall(x, y, direction)) {
+                direction = 0;
+            }
         }
 
         /* Update the sprite object */
         sprite.update(time);
+
+        if (direction !== moveBuffer)
+            move(moveBuffer);
     };
 
     // The methods are returned as an object here.
@@ -104,6 +260,9 @@ const Player = function (ctx, x, y, gameArea) {
         slowDown: slowDown,
         getBoundingBox: sprite.getBoundingBox,
         draw: sprite.draw,
-        update: update
+        update: update,
+        isCollideWithWall: isCollideWithWall,
+        eatDot: eatDot,
+        eatPowerDot: eatPowerDot,
     };
 };
--- public/scripts/game.js
+++ public/scripts/game.js
@@ -1,5 +1,13 @@
+//sound effects - do at the end
+const gameOverSound = new Audio("src/sounds/gameOver.wav");
+const gameWinSound = new Audio("src/sounds/gameWin.wav");
+const wakaSound = new Audio("src/sounds/waka.wav");
+const powerDotSound = new Audio("src/sounds/power_dot.wav");
+const eatGhostSound = new Audio("src/sounds/eat_ghost.wav");
+
 function gameLoop() {
   createBoard(); //in map.js
+  //document.getElementById("scores").value = scores;
   //checkGameOver(); //in checkGameStatus.js
   //checkGameWin(); //in checkGameStatus.js
 }
