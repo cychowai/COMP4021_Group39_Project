098941ed2ada375da3fd7550d7373670e1deb812
cychowai <71240864+cychowai@users.noreply.github.com>
2022-05-10 16:47:13 +0800

    Revert "Merge branch 'main' into jimmy-signup"
    This reverts commit 597451db0644be7c055063a118cd143fec97df30, reversing
    changes made to b6fd006d5812d04de065ae82fc6482238bd943a7.

--- public/frontPage.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-  $("#startGame").on("click", function () {
-    /* Hide the front page */
-    $("#frontPage").hide();
-    $("#mainBody").css("background-image", "none");
-    $("#container").show();
-  });
-});
